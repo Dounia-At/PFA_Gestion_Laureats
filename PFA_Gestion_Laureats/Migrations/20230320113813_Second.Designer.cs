8e3ba4254ddd308e9926b803139bc5bfa38395d6
Dounia-At <atoini.dounia@gmail.com>
2023-03-20 11:41:25 +0000

    Add-Migration Second

--- PFA_Gestion_Laureats/Migrations/20230320113813_Second.Designer.cs
+++ PFA_Gestion_Laureats/Migrations/20230320113813_Second.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PFA_Gestion_Laureats.Models;
 
@@ -11,9 +12,11 @@ using PFA_Gestion_Laureats.Models;
 namespace PFA_Gestion_Laureats.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20230320113813_Second")]
+    partial class Second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
